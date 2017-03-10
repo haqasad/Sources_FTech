bb1a0cbcbe13826068b896e3963bf4e7c645a718
haqasad <haq.asad000@gmail.com>
2017-03-10 06:10:22 -0500

    Project_ClassFeb5Java: Initial Commit

---
+++ FutureTechGitRepository/ClassFeb5Java/src/haq/asad/sum/class0205.java
@@ -0,0 +1,25 @@
+package haq.asad.sum;
+
+public class class0205 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	int a = 8;
+		int b = 2;
+		System.out.println(sumx(a,b));*/
+		int count = 0;
+		int[] myNum = {10, 10, 10, 10, 10};
+		
+		for (int i=0; i<myNum.length; i++) {
+			count += myNum[i];
+		}
+		
+		System.out.println(count);
+
+	}
+	
+	/*public static int sumx(int a, int b) {
+		return a+b;
+	}*/
+
+}
